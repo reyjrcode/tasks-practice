3f4b24e9b24859f23dd754dd447cc5e1a0f5d490
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-08-20 19:27:33 +0800

    update resource

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 // php artisan route:list
 Route::apiResource('tasks',TaskController::class)->only([
-    'index','show','store'
+    'index','show','store','update'
 ]);
