8eeee88d6cef148073d53fa33c0b46a35b7ca583
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-08-20 20:29:34 +0800

    pagination

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //php artisan make:factory TaskFactory --model=Task
+            // php artisan tinker
+            // Task::factory(50)->create()
+            // {{DOMAIN}}/api/tasks?page=2
+            'title'=> $this->faker->sentence(),
+            'is_done'=> false,
+        ];
+    }
+}
