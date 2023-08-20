66cefefa9a9ae1a7ffad18173a01f73c2c068043
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-08-20 22:44:02 +0800

    protecting routes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class Task extends Model
 {
@@ -23,5 +25,11 @@ class Task extends Model
     public function creator(): BelongsTo
     {
         return $this->belongsTo(User::class, 'creator_id');
+    }
+    protected static function booted(): void
+    {
+        static::addGlobalScope('creator', function (Builder $builder) {
+            $builder->where('creator_id', Auth::id());
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login',[AuthController::class,'login']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 // php artisan route:list
-Route::apiResource('tasks',TaskController::class);
+// Route::apiResource('tasks',TaskController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+});
