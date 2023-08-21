e6da2570d4f1618440bf4b9aab5c478dc8f8bb4b
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-08-21 14:08:13 +0800

    creating and updating project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
+
+use App\Http\Requests\StorePorjectRequest;
+
+use App\Http\Requests\UpdatePorjectRequest;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
     //
+    public function store(StorePorjectRequest $request)
+    {
+        $validated = $request->validated();
+
+        $project = Auth::user()->projects()->create($validated);
+
+        return new ProjectResource($project);
+    }
+
+    public function update(UpdatePorjectRequest $request, Project $project)
+    {
+        $validated = $request->validated();
+
+        $project->update($validated);
+
+        return new ProjectResource($project);
+    }
 }
