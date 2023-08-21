ae2360f6c19b11c30cc943144f05590da2e1021b
ReyAlcala <rey.alcalajr.code@gmail.com>
2023-08-21 20:32:04 +0800

    attach members to a project

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -10,10 +10,26 @@ class MembersController extends Controller
 {
     //php artisan make:controller MembersController
     // php artisan route:list
+    // {{DOMAIN}}/api/projects/1/members
     public function index(Request $request, Project $project)
     {
         $members = $project->members;
 
         return new UserCollection($members);
+    }
+    public function store(Request $request, Project $project)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        $project->members()->syncWithoutDetaching([$request->user_id]);
+
+        $members = $project->members;
+
+        return new UserCollection($members);
+        // php artisan tinker
+        // User::factory()->create()
+        
     }
 }
